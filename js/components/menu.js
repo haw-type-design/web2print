9246e678f813f37c9aadc4e435c7f0cc1f8b62a1
simonthi <github@simonthiefes.de>
2023-11-22 17:12:29 +0100

    Prep for week 5

--- js/components/menu.js
+++ js/components/menu.js
@@ -11,6 +11,7 @@ class Menu extends HTMLElement {
                 <a href="./#week-2">Week 2</a>
                 <a href="./#week-3">Week 3</a>
                 <a href="./#week-4">Week 4</a>
+                <a href="./#week-5">Week 5</a>
             </div>
             <div class="other">
                 <a href="./how-to.html">How Tos</a>
