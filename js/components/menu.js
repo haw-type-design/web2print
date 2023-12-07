d5801a799fb367429573bf806b3d38474e3f5717
simonthi <github@simonthiefes.de>
2023-12-07 09:51:28 +0100

    Update for week 7

--- js/components/menu.js
+++ js/components/menu.js
@@ -13,6 +13,7 @@ class Menu extends HTMLElement {
                 <a href="./#week-4">Week 4</a>
                 <a href="./#week-5">Week 5</a>
                 <a href="./#week-6">Week 6</a>
+                <a href="./#week-7">Week 7</a>
             </div>
             <div class="other">
                 <a href="./how-to.html">How Tos</a>
